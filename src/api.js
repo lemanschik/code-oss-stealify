8eeea8f72c6852db66fdd69c68f61383c9c56d10
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-01-06 16:11:52 +0100

    Update and rename api.ts to api.js

--- src/api.js
+++ src/api.js
@@ -1,16 +1,15 @@
-import * as path from "path";
 import { SWING_FILE } from "./extension-settings.js";
 import { getCandidateMarkupFilenames } from "./preview/languages/markup";
 
 export const api = {
-  isSwing(files: string[]) {
+  isComponent(files = ['']) {
     return (
       files.includes(".block") ||
       files.includes(SWING_FILE) ||
       files.some((file) => getCandidateMarkupFilenames().includes(file)) ||
       files.includes("scripts") ||
       (files.includes("script.js") &&
-        files.some((file) => path.extname(file) === ".markdown"))
+        files.some((file) => file.endsWith(".markdown")))
     );
   },
 };
